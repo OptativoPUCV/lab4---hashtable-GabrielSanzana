a4fe4403c66015a60e2bc9679b0041a94f90d8f9
Replit user <>
2023-04-10 22:22:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,20 +40,19 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-  size_t index = hash(key, map->capacity);
-  if(key==NULL || strcmp(key,map->buckets[index]->key) == 0)
+ if(key == NULL || containsKey(map, key)) // Verificar que la clave no sea nula y no esté repetida
     return;
-  if(map->buckets[index] == NULL) 
-  {
-    map->buckets[index] = createPair(key, value);
-     map->size++;
-    map->current = index;
+
+  size_t index = hash(key, map->capacity); // Aplicar la función hash a la clave
+
+  while(map->buckets[index] != NULL && map->buckets[index]->key != NULL) {
+    // Avanzar a la siguiente casilla disponible (nula o con par inválido)
+    index = (index + 1) % map->capacity;
   }
-  else
-  {
-    index++;
-    insertMap(map, (char *) &index, value);
-  }
+
+  map->buckets[index] = createPair(key, value); // Insertar el nuevo par en la casilla encontrada
+  map->size++; // Incrementar el tamaño del mapa
+  map->current = index; // Actualizar el índice current a la nueva posición
 }
 
 void enlarge(HashMap * map) {
